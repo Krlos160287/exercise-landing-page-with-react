66aaeba0f719a2b7327ba4cf2617422c653ae722
Krlos160287 <martigracia2018@gmail.com>
2021-07-16 19:31:02 +0000

    Come on

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -3,7 +3,11 @@ import React from "react";
 export const Card = () => {
 	return (
 		<div className="card" style="width: 18rem;">
-			<img src="..." className="card-img-top" alt="..." />
+			<img
+				src="https://www.caballerovillalpandoabogados.mx/img/our-office-8.jpg"
+				className="card-img-top"
+				alt="500x325"
+			/>
 			<div className="card-body">
 				<h5 className="card-title">Card title</h5>
 				<p className="card-text">
--- src/js/component/container.js
+++ src/js/component/container.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Card } from "./card";
 
 export const Container = () => {
 	return <p>hola</p>;
